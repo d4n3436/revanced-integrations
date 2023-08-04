66395ccd5efe250c60396da1d9496a962168dfba
kitadai31 <90122968+kitadai31@users.noreply.github.com>
2023-08-05 02:03:22 +0900

    Add `os-version-check` patch

---
+++ app/src/main/java/app/revanced/integrations/patches/utils/CheckOsVersionPatch.java
@@ -0,0 +1,24 @@
+package app.revanced.integrations.patches.utils;
+
+import static app.revanced.integrations.utils.StringRef.str;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.Build;
+
+public class CheckOsVersionPatch {
+    public static void checkOsVersion(Context context) {
+        if (Build.VERSION.SDK_INT < 26) return;  // No problem!
+
+        // show warning
+        Activity activity = (Activity) context;
+        new AlertDialog.Builder(activity)
+                .setTitle(str("dialog_title_warning"))
+                .setMessage(str("revanced_newer_android_warning_body"))
+                .setPositiveButton("OK", (dialog, id) -> activity.finish())
+                .setNeutralButton("Continue anyway", null)
+                .setCancelable(false)
+                .show();
+    }
+}
