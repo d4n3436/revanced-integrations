f9b32490975a7c0fb43b7dd0639546aaa7ca54dc
kitadai31 <90122968+kitadai31@users.noreply.github.com>
2024-03-28 16:19:29 +0900

    feat: Switch Vanced MicroG to ReVanced GmsCore

--- app/src/main/java/app/revanced/integrations/patches/utils/MicroGPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/utils/MicroGPatch.java
@@ -4,14 +4,16 @@ import static app.revanced.integrations.utils.StringRef.str;
 
 import android.content.ContentProviderClient;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.Build;
 import android.widget.Toast;
 
 public class MicroGPatch {
-    private static final String MICROG_VENDOR = "com.mgoogle";
+    private static final String MICROG_VENDOR = "app.revanced";
     private static final String MICROG_PACKAGE_NAME = MICROG_VENDOR + ".android.gms";
+    private static final String GMSCORE_DOWNLOAD_LINK = "https://github.com/ReVanced/GmsCore/releases/latest";
+    private static final String BACKGROUND_RUN_LINK = "https://github.com/kitadai31/revanced-patches-android6-7/wiki/Allow-GmsCore-run-in-background";
     private static final Uri VANCED_MICROG_PROVIDER = Uri.parse("content://" + MICROG_VENDOR + ".android.gsf.gservices/prefix");
 
     public static void checkAvailability(Context context) {
@@ -20,25 +22,23 @@ public class MicroGPatch {
         } catch (PackageManager.NameNotFoundException exception) {
             Toast.makeText(context, str("microg_not_installed_warning"), Toast.LENGTH_LONG).show();
             Toast.makeText(context, str("microg_not_installed_notice"), Toast.LENGTH_LONG).show();
+            startIntent(context, GMSCORE_DOWNLOAD_LINK);
             return;
         }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            // try-with-resources calls ContentProviderClient.close(), which requires API level 24
-            try (var client = context.getContentResolver().acquireContentProviderClient(VANCED_MICROG_PROVIDER)) {
-                if (client != null) return;
-                Toast.makeText(context, str("microg_not_running_warning"), Toast.LENGTH_LONG).show();
-            }
-
-        } else {
-            ContentProviderClient client = null;
-            try {
-                client = context.getContentResolver().acquireContentProviderClient(VANCED_MICROG_PROVIDER);
-                if (client != null) return;
-                Toast.makeText(context, str("microg_not_running_warning"), Toast.LENGTH_LONG).show();
-            } finally {
-                if (client != null) client.release();
-            }
+        ContentProviderClient client = null;
+        try {
+            client = context.getContentResolver().acquireContentProviderClient(VANCED_MICROG_PROVIDER);
+            if (client != null) return;
+            Toast.makeText(context, str("microg_not_running_warning"), Toast.LENGTH_LONG).show();
+            startIntent(context, BACKGROUND_RUN_LINK);
+        } finally {
+            if (client != null) client.release();
         }
+
+    }
+
+    private static void startIntent(Context context, String uriString) {
+        context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(uriString)));
     }
 }
