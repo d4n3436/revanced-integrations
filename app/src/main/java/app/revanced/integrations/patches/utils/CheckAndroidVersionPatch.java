bcabdfce65a5edf936d0368644767c438296b9fd
kitadai31 <90122968+kitadai31@users.noreply.github.com>
2024-06-04 21:04:18 +0900

    feat(os-version-check): Rename to `check-android-version`, and make it the default patch

--- app/src/main/java/app/revanced/integrations/patches/utils/CheckAndroidVersionPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/utils/CheckAndroidVersionPatch.java
@@ -6,16 +6,22 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.os.Build;
+import android.text.Html;
 
-public class CheckOsVersionPatch {
-    public static void checkOsVersion(Context context) {
+public class CheckAndroidVersionPatch {
+    public static void checkAndroidVersion(Context context) {
         if (Build.VERSION.SDK_INT < 26) return;  // No problem!
 
         // show warning
         Activity activity = (Activity) context;
         new AlertDialog.Builder(activity)
                 .setTitle(str("dialog_title_warning"))
-                .setMessage(str("revanced_newer_android_warning_body", Build.VERSION.RELEASE)) //%1$s
+                .setMessage(
+                        Html.fromHtml(
+                                str("revanced_newer_android_warning_body", Build.VERSION.RELEASE),
+                                Html.FROM_HTML_MODE_LEGACY
+                        )
+                ) //%1$s
                 .setPositiveButton("OK", (dialog, id) -> activity.finish())
                 .setNeutralButton("Continue anyway", null)
                 .setCancelable(false)
